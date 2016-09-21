967ca973be5f9170661b855f496b6a4fa5ae259b
RSPPA <anujstudy7@gmail.com>
2016-09-21 20:40:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-1',function(req, res) {
+   res.send('this is my first article'); 
+});
+
+app.get('/artical-2',function(req, res) {
+   res.send('this is my second article'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
