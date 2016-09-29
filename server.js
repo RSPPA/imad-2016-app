3e6d4a7e5454a7c8e84690631a3067cd7b6c18a1
RSPPA <anujstudy7@gmail.com>
2016-09-29 11:50:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,8 +123,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/1(1).png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '1(1).png'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
