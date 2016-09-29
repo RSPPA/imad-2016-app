b32795a7da356e680501e0c8b11ef88f56ddc01d
RSPPA <anujstudy7@gmail.com>
2016-09-29 11:02:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,17 +98,14 @@ app.get('/:articleName',function(req, res) {
    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-three', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
+
 
 app.get('/rao-shaab', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'rao-shaab.html'));
 });
 
-app.get('/article-two', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+
  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
