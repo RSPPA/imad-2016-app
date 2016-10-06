369658660de2190d9d20eadd2824d10798d09e85
RSPPA <anujstudy7@gmail.com>
2016-10-06 16:03:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,8 +111,8 @@ app.get('/:articleName',function(req, res) {
 });
 
 var counter=0;
-app.get('/counter',function(req,res) {
-    counter = counter +1;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
     res.send(counter.toString());
     });
 
